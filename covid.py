f4ac9f535d4e2621f5dba55bb0895124cd767eff
adrianboza <40300535+adrianboza@users.noreply.github.com>
2020-03-31 01:19:54 -0500

    ...

--- covid.py
+++ covid.py
@@ -1,3 +1,28 @@
-name=input()
-  print("hola",name)
-  print("CS")
+genName = ["AY274119","AY278488.2","MN908947","MN988668","MN988669"]
+
+def virus(genomaName):
+  gen = open(genomaName + ".txt","r")
+  codigo = gen.read()
+  gen.close()
+  return codigo
+
+virus = [virus(genName[0]),virus(genName[1]),virus(genName[2]),virus(genName[3]),virus(genName[4])]
+
+def comparar(v1, v2):
+  p = 0
+  for i in range(min(len(v1),len(v2))):
+    if(v1[i]==v2[i]):
+      p +=1
+  return round(p*100/len(v1),1)
+
+for i in genName:
+    print("          ", i, end = "")
+print("\n")
+for j in range(len(genName)):
+    if j == 1:
+        print(genName[j], end = " ")
+    else:
+        print(genName[j], end = "   ")
+    for i in range(len(genName)):
+        print(round(comparar(virus[j],virus[i]),2),"               ", end = "")
+    print("\n")
